7ab655e1ae5aa45ce637528a1dc1c9f800a91144
ColterH <colterhawkstetler@gmail.com>
2024-03-03 18:56:55 -0800

    Remove node_modules from Git tracking

--- Frontend/rollup.config.mjs
+++ Frontend/rollup.config.mjs
@@ -17,34 +17,29 @@ export default {
   },
   plugins: [
     svelte({
-      // enable run-time checks when not in production
-      dev: !production,
-      // we'll extract any component CSS out into a separate file - better for performance
-      css: css => {
-        css.write('public/build/bundle.css');
+      compilerOptions: {
+        // enable run-time checks when not in production
+        dev: !production,
       },
+      // Correctly placed outside `compilerOptions`
+      emitCss: true, // ensure you're emitting CSS as an asset for rollup-plugin-css-only to process
     }),
-    // we'll need to convert our .css files to be usable in JavaScript
+    // Handle CSS extraction separately
     css({ output: 'bundle.css' }),
 
-    // If you have external dependencies installed from npm, you'll most likely need these plugins. In some cases you'll need additional configuration - 
-    // Consult the documentation for details: https://github.com/rollup/plugins/tree/master/packages/commonjs
     resolve({
       browser: true,
       dedupe: ['svelte']
     }),
 
-    // In dev mode, call `npm run start` once the bundle has been generated
     !production && serve({
       contentBase: 'public',
       historyApiFallback: true,
       port: 5000,
     }),
 
-    // Watch the `public` directory and refresh the browser on changes when not in production
     !production && livereload('public'),
 
-    // If we're building for production (npm run build instead of npm run dev), minify
     production && terser()
   ],
   watch: {
